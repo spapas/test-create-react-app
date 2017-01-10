892dc4f88a34d3e77bc5429d800d7a650efc8e0c
Serafeim Papastefanos <spapas@gmail.com>
2017-01-10 13:18:57 +0200

    Small changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
+import { Grid, Navbar, Nav, NavItem, NavDropdown, MenuItem, Jumbotron, Button, Row, Col } from 'react-bootstrap';
+
+import Footer from './components/Footer';
+
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
+export default () => <div>
+  <Navbar inverse fixedTop collapseOnSelect>
+    <Grid>
+      <Navbar.Header>
+        <Navbar.Brand>
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="#">Link</NavItem>
+          <NavItem eventKey={2} href="#">Link</NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Grid>
+  </Navbar>
+  <Jumbotron>
+    <Grid>
+      <h1>Welcome to React</h1>
+      <Row >
+        <Col md={6}>
+          <Button
+            bsStyle="success"
+            href="http://react-bootstrap.github.io/components.html"
+            target="_blank">
+            View React Bootstrap Docs
+          </Button>
+        </Col>
+        <Col md={1}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </Col>
+      </Row>
+    </Grid>
+  </Jumbotron>
+  <Footer />
+</div>
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Grid, Row, Col, Panel } from 'react-bootstrap';
+import './Footer.css';
+
+export default () => <div className="footer text-center">
+    Hello, footer !
+</div>
+
